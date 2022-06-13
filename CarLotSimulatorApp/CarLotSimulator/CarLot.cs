1d0a931cf454f83ce84b8090574ac205fd3270ab
Sokpagna Lim <sokpagnalim@gmail.com>
2022-06-13 07:58:37 -0700

    CompleteCarLotExercise

---
+++ CarLotSimulatorApp/CarLotSimulator/CarLot.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarLotSimulator
+{
+    public class CarLot
+    {
+        public List<Car> CarLots = new List<Car>();
+        // public List<Car> Carlots2 {get; set} = new List<Car>();
+    }
+}
